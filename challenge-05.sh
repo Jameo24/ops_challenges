75b98b38f56450b8c6ac3c02fbf1bb44b0f1c5dd
James <jamesmoring@ymail.com>
2022-08-01 05:54:12 +0000

    add challenge-05.sh

---
+++ challenge-05.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: James Moring
+# Date of latest revision: 07/30/2022
+# Purpose: Print a string to the terminal
+
+# Main
+
+
+
+# End
